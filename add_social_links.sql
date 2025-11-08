7e0a90f8cf254de96dc53034a8e99da705f98b4a
Sabarinath P <sabarinathp@Sabarinaths-MacBook-Air.local>
2025-11-09 01:20:08 +0530

    files fixed

--- add_social_links.sql
+++
@@ -1,6 +0,0 @@
--- Add social media links to User table
--- Migration: add_social_links
--- Date: 2025-11-09
-
-ALTER TABLE `User` ADD COLUMN `facebookLink` VARCHAR(191) NULL;
-ALTER TABLE `User` ADD COLUMN `instagramLink` VARCHAR(191) NULL;
